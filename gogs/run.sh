b68a1419a9be0f17777a4c7b5f657a93b73c37a5
Mathieu POUSSE <mathieu.pousse@zenika.com>
2016-03-06 08:47:37 +0100

    more tools with compose
    Signed-off-by: Mathieu POUSSE <mathieu.pousse@zenika.com>

--- gogs/run.sh
+++ gogs/run.sh
@@ -2,14 +2,13 @@
 
 BASE_DIR=$(cd $(dirname $0); pwd)
 
-docker rm -f $(docker ps -aq --filter=name=gogs) || true
+docker rm -f $(docker ps -aq --filter=name=gogs) 2>/dev/null || true
 
-docker run --name=gogs \
+docker run -d --name=gogs \
            -l traefik.backend=gogs \
            -l traefik.port=3000 \
            -l traefik.frontend.rule=PathPrefixStrip \
            -l traefik.frontend.value=/gogs \
            -l traefik.frontend.passHostHeader=true \
-           --volume $BASE_DIR/data/gogs/conf:/data/gogs/conf \
-           --net ci-containers \
+	   --volume $BASE_DIR/data/gogs/conf:/data/gogs/conf \
            gogs/gogs
