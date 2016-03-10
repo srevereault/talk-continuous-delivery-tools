52530dc6d341ac6c566ad5cdf9407287d3e96ee1
Mathieu POUSSE <mathieu-pousse@users.noreply.github.com>
2016-03-10 23:15:50 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,7 @@ groupadd docker || true
 gpasswd -a $(logname) docker
 
 echo "Installing docker-compose..."
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s-uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
 chmod +x /usr/local/bin/docker-compose /etc/bash_completion.d/docker-compose
 
